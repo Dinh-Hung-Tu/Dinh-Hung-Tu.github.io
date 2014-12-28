5cb793c982274f097d3a2ea41c217ea2a9ca3443
Dinh-Hung-Tu <dinhhungtu@gmail.com>
2014-12-28 16:18:05 +0800

    add wordnik api to elaborate the keyword

--- gre/flashcard.js
+++ gre/flashcard.js
@@ -197,3 +197,14 @@ function initialization()
 	submitButton.disabled = true;
 	nextButton.disabled = true;
 }
+//Add the function for elaboration
+function elaborate()
+{
+	word = document.getElementById('main').innerHTML;
+	getSynonyms ("word", function (outwords) {
+				var s = "Synonym :";
+				for (var i = 0; i <outwords.length; i++) {
+					s += outwords [i] + " "
+					}
+				$("#idElaborate").html ("<ul>" + s + "</ul>");	
+}
